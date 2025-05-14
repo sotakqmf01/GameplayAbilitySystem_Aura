cab22b86e9bf4773febfbe7c17f17298dbb51f3a
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-05-14 12:40:36 +0900

    [Add] AuraCharacter, EnemyCharacter

---
+++ Source/Aura/Character/Aura/AuraCharacter.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../AuraBaseCharacter.h"
+#include "AuraCharacter.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacter : public AAuraBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
